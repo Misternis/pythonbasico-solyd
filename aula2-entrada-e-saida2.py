ee772f46229c7d273a58a56f9144431bdc2c67a1
misternis <viniciosraymundo@gmail.com>
2022-07-26 18:32:50 -0300

    Criado nome, idade, altura e seus types... feito também interação para teste de conhecimento.

---
+++ aula2-entrada-e-saida2.py
@@ -0,0 +1,25 @@
+nome = 'Vinicios'
+idade = 28
+altura = 1.71
+
+tipo_nome = type (nome)
+tipo_idade = type (idade)
+tipo_altura = type (altura)
+
+print(nome)
+print(idade)
+print(altura)
+
+print(tipo_nome)
+print(tipo_idade)
+print(tipo_altura)
+
+print('Resultado final' ,nome , 'tem', idade, 'anos e', altura, 'de altura')
+
+
+
+qual_nome = input('E você qual o seu Nome ')
+qual_idade = input('E a sua Idade ')
+qual_altura = input('Beleza, ultima qual a sua Altura ')
+
+print(qual_nome,'tem', qual_idade, 'anos e', qual_altura)
